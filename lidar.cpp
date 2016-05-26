aa3655a698894f0213b97aa895bfea7b5a100c41
tigerh <tigerrun1998@gmail.com>
2016-05-26 00:21:22 +0000

    Cleaned up some unnecessary code and added slightly better comments.

--- lidar.cpp
+++ lidar.cpp
@@ -55,11 +55,6 @@ int pcount;
 
 GLubyte out_tex_buf[CAMERA_WIDTH * CAMERA_HEIGHT * 4];
 
-unsigned short binToHex(unsigned char value);
-unsigned char hexToBin(unsigned short value);
-
-//FILE* out_fd;
-
 int main(int argc, const char **argv)
 {
   //Init the laser output
@@ -90,11 +85,6 @@ int main(int argc, const char **argv)
   cfsetispeed(&tio,B115200);            // 115200 baud
   
   tcsetattr(tty_fd,TCSANOW,&tio);
-
-  char c = ' ';
-
-  //Setting up file output
-  //out_fd = fopen("out.txt","a+");
 
   //init graphics and the camera
   InitGraphics();
@@ -173,9 +163,7 @@ int main(int argc, const char **argv)
                ((unsigned char*)(data_buf + 500))[3]);
 
       //Displaying the found value if it is above a threshold
-      //Also calculating actual positions
-//if (read(tty_fd,&c,1)>0)  write(tty_fd,&c,1);
-//dprintf(tty_fd,"1");
+      //Also output to serial port
       pcount = 0;
       for(int j = 0;j < CAMERA_WIDTH;j++) {
         if(max_value[j] > 14) {
@@ -187,21 +175,8 @@ int main(int argc, const char **argv)
           buf[0] = 0;
           buf[1] = output;
           write(tty_fd,buf,2);
-          
-          //Calculating actual position
-          //dpos[pcount] = VD * H * 1.0f / (max_index[j] - CAMERA_HEIGHT / 2);
-          //xpos[pcount] = dpos[pcount] * (j - CAMERA_WIDTH / 2) / VD;
-          //pcount++;
-
-          //Writing out position to serial port
-          //dprintf(tty_fd,"%i %i ",(int)xpos[pcount - 1],(int)dpos[pcount - 1]);
-
-          //write(tty_fd,buf,2);
-          
-          //fprintf(out_fd,"%i ",max_index[j]);
         }
         else {
-          //fprintf(out_fd,"-1 ");
           buf[0] = 255;
           buf[1] = 254;
           write(tty_fd,buf,2);
@@ -211,8 +186,6 @@ int main(int argc, const char **argv)
       for(int j = 0;j < CAMERA_WIDTH;j++) {
           out_tex_buf[((max_index[j] * CAMERA_WIDTH) + j) * 4 + 1] = 0;
       }
-      //fprintf(out_fd,"\n");
-      //dprintf(tty_fd,"\n");
       buf[0] = 255;
       buf[1] = 255;
       write(tty_fd,buf,2);
@@ -222,7 +195,6 @@ int main(int argc, const char **argv)
      
     gettimeofday(&time, NULL);
     new_time = time.tv_sec * 1000000 + time.tv_usec;
-    //mvprintw(0,0,"uS/Frame: %i\n",new_time - old_time);
     int max = (new_time - old_time) / 1000;
     old_time = new_time;
 
@@ -232,7 +204,6 @@ int main(int argc, const char **argv)
 
   endwin();
   StopCamera();
-  //fclose(out_fd);
   close(tty_fd);
   return 0;
 }
@@ -280,21 +251,4 @@ void revokeRoot()
 
   if (geteuid () == 0)                  // Running setuid root
     seteuid (getuid ()) ;               // Change effective uid to the uid of the caller
-}
-
-//In these two functions, the two ascii values are crammed into the short
-unsigned short binToHex(unsigned char value) {
-  unsigned char top = value >> 4;
-  unsigned char bottom = value & 0x0F;
-  unsigned short result;
-  ((char*)(&result))[1] = ((top <= 9) ? (top + '0') : (top + 'A' - 0x0A));
-  ((char*)(&result))[0] = ((bottom <= 9) ? (bottom + '0') : (bottom + 'A' - 0x0A));
-  return result;
-}
-
-unsigned char hexToBin(unsigned short value) {
-  unsigned char top = value >> 8;
-  unsigned char bottom = value & 0xFF;
-  return (((top <= '9') ? (top - '0') : (top - 'A' + 0x0A)) << 4) |
-    ((bottom <= '9') ? (bottom - '0') : (bottom - 'A' + 0x0A));
 }
